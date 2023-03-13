60940dde8dde39c01b3235cb0cd3620462afa9e8
Axelc <axelpinargoteortega@gmail.com>
2023-03-13 16:14:48 -0500

    propiedades de coneccion aql

--- src/main/java/com/registro/usuarios/loginv2/entity/User.java
+++ src/main/java/com/registro/usuarios/loginv2/entity/User.java
@@ -1,5 +1,105 @@
 package com.registro.usuarios.loginv2.entity;
 
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private long id;
+
+	@Column(name = "firstName")
+	private String firstName;
+
+	@Column(name = "lastName")
+	private String lastName;
+
+	@Column(unique = true, nullable = true)
+	private String email;
+
+	private String password;
+	private Collection<Rol> roles;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Rol> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Rol> roles) {
+		this.roles = roles;
+	}
+
+	public User(long id, String firstName, String lastName, String email, String password, Collection<Rol> roles) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public User(String firstName, String lastName, String email, String password, Collection<Rol> roles) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public User() {
+		super();
+	}
+	
+	
 
 }
